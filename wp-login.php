a6e4f9d930dd26ba3b3e78c85164aaa106403b6b
thanhtung-kaka <85726557+thanhtung-kaka@users.noreply.github.com>
2021-06-12 09:17:27 +0700

    Intial Generated Site

--- wp-login.php
+++ wp-login.php
@@ -2,7 +2,7 @@
 <html lang="vi">
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<title>Đăng nhập &lsaquo; My Wordpress &#8212; WordPress</title>
+	<title>Đăng nhập &lsaquo; mywordpress &#8212; WordPress</title>
 	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
 <link rel="dns-prefetch" href="//s.w.org">
 <link rel="stylesheet" id="dashicons-css" href="https://thanhtung-kaka.github.io/thanhtung0104/wp-includes/css/dashicons.min.css?ver=5.7.2" media="all">
@@ -51,7 +51,7 @@
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
 				<p id="backtoblog">
-			<a href="https://thanhtung-kaka.github.io/thanhtung0104/">&larr; Quay lại My Wordpress</a>		</p>
+			<a href="https://thanhtung-kaka.github.io/thanhtung0104/">&larr; Quay lại mywordpress</a>		</p>
 			</div>
 	<script src="https://thanhtung-kaka.github.io/thanhtung0104/wp-includes/js/jquery/jquery.min.js?ver=3.5.1" id="jquery-core-js"></script>
 <script src="https://thanhtung-kaka.github.io/thanhtung0104/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
@@ -85,7 +85,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/myproject\/wordpress\/wp-admin\/admin-aj
 </script>
 <script src="https://thanhtung-kaka.github.io/thanhtung0104/wp-includes/js/wp-util.min.js?ver=5.7.2" id="wp-util-js"></script>
 <script id="user-profile-js-extra">
-var userProfileL10n = {"user_id":"0","nonce":"7d35acb7b9"};
+var userProfileL10n = {"user_id":"0","nonce":"18373376c5"};
 </script>
 <script id="user-profile-js-translations">
 ( function( domain, translations ) {
